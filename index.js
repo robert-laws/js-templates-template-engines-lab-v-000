f7ce5b55fbdefe577b4266f5a3acdfed5218b239
Robert Laws <roblaws@me.com>
2018-04-20 18:39:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createPost() {
   var pageTemplate = _.template(document.getElementById('page-template').innerHTML);
   var postTemplate = _.template(document.getElementById('post-tempate').innerHTML);
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
-  
+
   var postTitle = document.getElementById("postTitle").value;
   var postAuthor = document.getElementById("postAuthor").value;
   var post = document.getElementById("postBody").value;
