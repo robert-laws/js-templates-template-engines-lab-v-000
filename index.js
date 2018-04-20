0d56f6948ca83dbbbc3b00825252eab226182c94
Robert Laws <roblaws@me.com>
2018-04-20 18:45:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function createPost() {
   // create template functions
-  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var pageTemplate = _.template(document.getElementById('page-template').innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
